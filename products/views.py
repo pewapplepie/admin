54987452e545899b3b62bb84a09c1a27f4d0e2ae
Jer-Shen Chen <jschen1217@gmail.com>
2023-12-17 23:19:23 -0500

    update

--- products/views.py
+++ products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
-
+from rest_framwork import viewsets
 # Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+
+    def create(self,request):
+        pass
